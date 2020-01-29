d6765bc82fdf4bfd4c725db9c611e3e0a5a69525
vlad.colceriu <vlad-doru.colceriu@klarna.com>
2020-01-29 17:43:00 +0200

    KAF-7 Allowed for remote debugging to occur in the Docker containers.

--- src/main/java/com/example/kafka/resource/SimpleResource.java
+++ src/main/java/com/example/kafka/resource/SimpleResource.java
@@ -1,6 +1,5 @@
 package com.example.kafka.resource;
 
-import com.example.kafka.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
